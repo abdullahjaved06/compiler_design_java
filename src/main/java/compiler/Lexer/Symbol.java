47980fad3a4ab5d3171ac5ceeffdd1fb36159573
abdullahjaved06 <javedabdullah963@gmail.com>
2026-02-11 15:58:31 +0100

    added symbol class

--- src/main/java/compiler/Lexer/Symbol.java
+++ src/main/java/compiler/Lexer/Symbol.java
@@ -1,4 +1,33 @@
 package compiler.Lexer;
 
 public class Symbol {
+    private TokenType type;
+    private String value;
+    private int line;
+    private int column;
+
+    //constructor - creates a new symbol
+    public Symbol(TokenType type, String value, int line, int column) {
+        this.type = type;
+        this.value = value;
+        this.line  = line;
+        this.column = column;
+    }
+    // Getter functions. it will allow other classses to read thedata.
+    public TokenType getType() {
+        return type;
+    }
+    public String getValue(){
+        return value;
+    }
+    public int getLine(){
+        return line;
+    }
+    public int getColumn() {
+        return column;
+    }
+    @override
+    public String toString() {
+        return "<" + type + "," + value + ">";
+    }
 }
